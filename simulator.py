0d8e54cd1c7ca02d2113b0adfd1b98fa115252e6
matts <mattsheldon914@gmail.com>
2026-02-08 12:43:06 +0000

    making sure in-trip rewards are observed

--- simulator.py
+++ simulator.py
@@ -139,6 +139,7 @@ class Simulator:
         #end_time = request.time + self.grid.get_travel_time(request.start_cluster, request.end_cluster, self.get_period())
         end_time = request.time + self.empirical_tt.get_sample(self.get_period(), request.start_cluster, request.end_cluster)
 
+        self.transiters[event.start_cluster][event.cluster][event._class] += 1
         arrival = Arrival(end_time, request.start_cluster, request.end_cluster, driver_class)
         heapq.heappush(self.next_events, (end_time, "a", arrival))
 
@@ -186,8 +187,14 @@ class Simulator:
         start = event.start_cluster
         end = event.cluster
 
+        self.transiters[event.start_cluster][event.cluster][event._class] -= 1
+
+        cost = self.grid.distance_costs[period][event.start_cluster][event.cluster]
+
         subsidy = self.controller.get_subsidy(period, driver_class, start, end)
         self.models[self.get_period()][driver_class].observe_s(start, end, subsidy)
+
+        self.observer.observe_reward(-cost, -subsidy)
 
         # let the driver decide where to spawn
         self.decide(event.cluster, driver_class)
