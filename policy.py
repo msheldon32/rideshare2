c9bc37cc3ed5654f90a9ae1324a4ea666d7da735
matts <mattsheldon914@gmail.com>
2026-02-09 11:48:15 +0000

    further fixes

--- policy.py
+++ policy.py
@@ -1,8 +1,8 @@
 
-def get_cvxpy_prob(self, input_vehicle_rewards="model_config"):
-    if type(input_vehicle_rewards) == str:
-        input_vehicle_rewards = self.model_config.vehicle_rewards
-    n_loc = len(self.model_config.locations)
+def get_cvxpy_prob(model_config, input_vehicle_rewards=None):
+    if input_vehicle_rewards is None:
+        input_vehicle_rewards = model_config.vehicle_rewards
+    n_loc = len(model_config.locations)
 
     # arrivals into queue: arrivals into the queue at location i, from vehicles with origin k
     arrivals_into_queue = cp.Variable((n_loc, n_loc), nonneg=True)
@@ -23,19 +23,19 @@ def get_cvxpy_prob(self, input_vehicle_rewards="model_config"):
 
     for k in range(n_loc):
         for i in range(n_loc):
-            expected_tt_difference = -sum([self.model_config.customer_transitions[i][j] * self.model_config.get_prepaid_cost(k, i, j) for j in range(n_loc)])
-            vehicle_utilities[k][i] = (expected_tt_difference + vehicle_rewards[k][i]) / self.model_config.reservation
+            expected_tt_difference = -sum([model_config.customer_transitions[i][j] * model_config.get_prepaid_cost(k, i, j) for j in range(n_loc)])
+            vehicle_utilities[k][i] = (expected_tt_difference + vehicle_rewards[k][i]) / model_config.reservation
 
     # objective function: minimize the travel time multiplied by the flow minus the log of (service rate - total arrival rate across all origins)
     obj = 0
     for k in range(n_loc):
         for i in range(n_loc):
             for j in range(n_loc):
-                travel_cost = self.model_config.get_prepaid_cost(k, i, j) / self.model_config.reservation
+                travel_cost = model_config.get_prepaid_cost(k, i, j) / model_config.reservation
                 obj += travel_cost * flows_between_locations[k][i,j]
 
     for i in range(n_loc):
-        obj -= cp.log(self.model_config.service_rates[i] - total_arrival_rates[i])
+        obj -= cp.log(model_config.service_rates[i] - total_arrival_rates[i])
 
     for i in range(n_loc):
         for k in range(n_loc):
@@ -53,9 +53,9 @@ def get_cvxpy_prob(self, input_vehicle_rewards="model_config"):
     # 2. flow conservation
     for k in range(n_loc):
         for i in range(n_loc):
-            outside_arrival_rate = self.model_config.arrival_rates[i] if i == k else 0
+            outside_arrival_rate = model_config.arrival_rates[i] if i == k else 0
 
-            vehicle_returns = (1 - self.model_config.exit_prob) * cp.sum([arrivals_into_queue[k, j] * self.model_config.customer_transitions[j][i] for j in range(n_loc)])
+            vehicle_returns = (1 - model_config.exit_prob) * cp.sum([arrivals_into_queue[k, j] * model_config.customer_transitions[j][i] for j in range(n_loc)])
 
             constraints.append(cp.sum(flows_between_locations[k][:, i]) - cp.sum(flows_between_locations[k][i, :]) + outside_arrival_rate + vehicle_returns == arrivals_into_queue[k,i] + balk_rate[k,i])
 
@@ -63,11 +63,11 @@ def get_cvxpy_prob(self, input_vehicle_rewards="model_config"):
 
     return [prob, total_arrival_rates, vehicle_rewards, arrivals_into_queue, flows_between_locations, balk_rate]
 
-def get_policies(self, input_vehicle_rewards="model_config"):
-    prob, total_arrival_rates, vehicle_rewards, arrivals_into_queue, flows_between_locations, balk_rate = self.get_cvxpy_prob(input_vehicle_rewards)
+def get_policies(model_config, input_vehicle_rewards=None):
+    prob, total_arrival_rates, vehicle_rewards, arrivals_into_queue, flows_between_locations, balk_rate = get_cvxpy_prob(model_config, input_vehicle_rewards)
     prob.solve()
 
-    n_loc = len(self.model_config.locations)
+    n_loc = len(model_config.locations)
 
     # policies[k][i] is a list of n_loc + 1 probabilities:
     #   policies[k][i][j] for j != i: probability of relocating to location j
